08bc0d03371ed591dca14c99a40b6079b8027905
Darren Henderson <darrenhusa@yahoo.com>
2021-07-28 10:56:36 -0500

    Moving to tailwind.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,18 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/normalize.css">
+    <!-- <link rel="stylesheet" href="/css/normalize.css"> -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet"> 
     <title>CSS for Backend Devs - #2</title>
 </head>
 <body>
-    <div class="card">
+    <div class="card flex">
 
-        <div class="card-left">
+        <div class="card-left flex flex-col justify-between items-center text-center text-white">
 
-            <a href="#" class="card-skill-button">PHP</a>
+            <a href="#" class="card-skill-button rounded-full">PHP</a>
 
             <img src="https://laracasts.s3.amazonaws.com/series/thumbnails/10-techniques-for-cleaner-code.png?v=8" width="98" height="98">
 
@@ -25,20 +26,20 @@
             </span>
         </div>
 
-        <div class="card-right">
-            <h3 class="card-title"><a href="#">Testing Jargon</a></h3>
+        <div class="card-right flex flex-col">
+            <h3 class="card-title no-underline font-medium mt-0 mb-0"><a href="#">Testing Jargon</a></h3>
 
-            <p class="card-excerpt">
+            <p class="card-excerpt flex-1">
                 There's no two ways about it: terminology in the testing world is incredibly overwhelming. Let's fix that! Bit by bit, we'll break all of these confusing concepts down as best we can.
             </p>
 
-            <div class="card-meta">
-                <div>
+            <div class="card-meta flex">
+                <div class="flex">
                     <img src="https://image.flaticon.com/icons/svg/864/864685.svg"  width="15" height="15">
                     5 Lessons 
                 </div>
                 
-                <div>
+                <div class="flex">
                     <img src="https://image.flaticon.com/icons/svg/2088/2088617.svg"  width="15" height="15">
                     53:10 min
                 </div>
