a98ee8223bea661470e19c4b8da153de782162c9
Darren Henderson <darrenhusa@yahoo.com>
2021-07-28 08:57:00 -0500

    Add normalize.css. Add Open Sans Google font. Create html structure for card component.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet"> 
+    <title>CSS for Backend Devs - #2</title>
 </head>
 <body>
-    <h1>Hello</h1>
+    <div class="card">
+
+        <div class="card-left">
+
+            <a href="#">PHP</a>
+
+            <img src="" alt="">
+
+            <div class="card-difficulty">
+                Intermediate Difficulty
+            </div>
+        </div>
+
+        <div class="card-right">
+            <div class="card-title">
+                Testing Jargon
+            </div>
+
+            <div class="card-excerpt">
+                <p>There's no two ways about it Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam pariatur esse, beatae molestiae laboriosam ad, est officiis sequi tempore quidem eos porro amet non cum voluptates suscipit illum at quam!</p>
+            </div>
+
+            <div class="card-meta">
+                5 Lessons 53:10 min
+            </div>
+
+        </div>
+    </div>
     
 </body>
 </html>
