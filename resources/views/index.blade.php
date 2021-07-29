2a91e8866e190749ecf5582df04d843fecc3dd25
Darren Henderson <darrenh@ccsj.edu>
2021-07-28 19:24:39 -0500

    Finished "Refactoring to Utility Classes and Tailwind" video.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,41 +12,41 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet"> 
     <title>CSS for Backend Devs - #2</title>
 </head>
-<body>
-    <div class="card flex">
+<body class="flex items-center justify-center">
+    <div class="card flex rounded">
 
-        <div class="card-left flex flex-col justify-between items-center text-center text-white">
+        <div class="card-left flex flex-col justify-between p-6 items-center text-center text-white rounded">
 
-            <a href="#" class="card-skill-button rounded-full">PHP</a>
+            <a href="#" class="card-skill-button no-underline text-xs rounded-full py-1 px-6 text-inherit">PHP</a>
 
             <img src="https://laracasts.s3.amazonaws.com/series/thumbnails/10-techniques-for-cleaner-code.png?v=8" width="98" height="98">
 
-            <span class="card-difficulty">
+            <span class="card-difficulty text-xs">
                 Intermediate Difficulty
             </span>
         </div>
 
-        <div class="card-right flex flex-col">
-            <h3 class="card-title no-underline font-medium mt-0 mb-0"><a href="#">Testing Jargon</a></h3>
+        <div class="card-right flex flex-col p-6 text-gray-800">
+            <h3 class="card-title font-light text-3xl mb-3 text-black">
+                <a href="#" class="no-underline text-inherit">Testing Jargon</a>
+            </h3>
 
             <p class="card-excerpt flex-1">
                 There's no two ways about it: terminology in the testing world is incredibly overwhelming. Let's fix that! Bit by bit, we'll break all of these confusing concepts down as best we can.
             </p>
 
-            <div class="card-meta flex">
-                <div class="flex">
-                    <img src="https://image.flaticon.com/icons/svg/864/864685.svg"  width="15" height="15">
+            <div class="card-meta flex text-sm">
+                <div class="flex items-center mr-4">
+                    <img src="https://image.flaticon.com/icons/svg/864/864685.svg"  width="15" height="15" class="mr-2">
                     5 Lessons 
                 </div>
                 
-                <div class="flex">
-                    <img src="https://image.flaticon.com/icons/svg/2088/2088617.svg"  width="15" height="15">
+                <div class="flex items-center">
+                    <img src="https://image.flaticon.com/icons/svg/2088/2088617.svg"  width="15" height="15" class="mr-2">
                     53:10 min
                 </div>
             </div>
-
         </div>
     </div>
-    
 </body>
 </html>
